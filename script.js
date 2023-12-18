3933d6bf07eab073ee2a7677528bdee9a5b2c5f8
Ruby Hu <60735962+HuWenShin@users.noreply.github.com>
2023-12-18 16:41:30 +0800

    Create files and basic structure
    Created html, css and javascript files. Added basic html structure and
    broke down javascript functions.

---
+++ script.js
@@ -0,0 +1,37 @@
+const myLibrary = [harryPotter, breath, pridePrejudice];
+
+function Book(name, author, pages, read) {
+    this.name = name;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+const harryPotter = new Book("Harry Potter and the Sorcerer's Stone", "J.K. Rowling", 320, true);
+const breath = new Book("When Breath Becomes Air", "Paul Kalanithi", 228, true);
+const pridePrejudice = new Book("Pride and Prejudice", "Jane Austen", 496, false);
+
+//pop up form display
+function openForm() {
+    document.getElementById("popup").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("popup").style.display = "none";
+}
+
+
+//process form submitted from user and add book info into library array
+function addBookToLibrary() {
+    
+}
+
+//loop through array and display the books in cards
+function display() {
+    for (let book in myLibrary) {
+        document.getElementsByClassName(title).innerText = book.name;
+        document.getElementsByClassName(author).innerText = book.author;
+        document.getElementsByClassName(pages).innerText = book.pages;
+
+    }
+}
