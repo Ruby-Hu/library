409bc2660c95c5121c56d979d12cf09c49d4721a
Ruby Hu <60735962+Ruby-Hu@users.noreply.github.com>
2025-07-01 15:59:54 -0700

    refactor with class

--- script.js
+++ script.js
@@ -6,14 +6,80 @@ const submitBtn = document.getElementById("submitBtn");
 const readBtn = document.getElementsByClassName("read-button");
 const deleteBtn = document.getElementsByClassName("delete-button");
 
-function Book(name, author, pages, read) {
-    this.name = name;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+
+class Book {
+    constructor(name, author, pages, read) {
+        this.name = name;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
 }
 
-const myLibrary = [];
+class Library {
+    constructor() {
+        this.books = [];
+    }
+
+    addBook(book) {
+        this.books.push(book);
+    }
+
+    removeBook(index) {
+        this.books.splice(index, 1);
+        this.display();
+    }
+
+    getBook() {
+        return this.books;
+    }
+
+    toggleRead(index) {
+        this.books[index].read = !this.books[index].read;
+        this.display();
+    }
+
+    display() {
+        shelf.innerText = "";
+
+        this.books.forEach((book, index) => {
+            const card = document.createElement("div");
+            card.dataset.index = index;
+        
+            const title = document.createElement("h3");
+            title.innerText = book.name;
+            
+            const author = document.createElement("p");
+            author.innerText = `By ${book.author}`;
+            
+            const pages = document.createElement("p");
+            pages.innerText = `${book.pages} pages`;
+            
+            const readBtn = document.createElement("button");
+            readBtn.innerText = book.read ? "Read" : "Unread";
+            readBtn.classList.add(book.read ? "btn-read" : "btn-unread");
+            readBtn.addEventListener("click", () => this.toggleRead(index));
+
+            const deleteBtn = document.createElement("button");
+            deleteBtn.innerText = "Delete";
+            deleteBtn.addEventListener("click", () => this.removeBook(index));
+
+            const buttonGroup = document.createElement("div");
+            const infoGroup = document.createElement("div");
+
+            shelf.appendChild(card).className = "card";
+            card.appendChild(infoGroup).className = "info-group"
+            card.appendChild(buttonGroup).className = "button-group";
+            infoGroup.appendChild(title).className = "title";
+            infoGroup.appendChild(author).className = "author";
+            infoGroup.appendChild(pages).className = "pages";
+            buttonGroup.appendChild(readBtn);
+            buttonGroup.appendChild(deleteBtn).className = "delete-button";
+        });
+    }
+}
+
+const library = new Library();
 
 // pop up form display
 addBtn.addEventListener("click", () => {
@@ -25,91 +91,6 @@ closeBtn.addEventListener("click", () => {
 });
 
 
-// process form submitted from user and add book info into library array
-function addBookToLibrary(title, author, pages, read) {
-    const newBook = new Book(title, author, pages, read);
-    myLibrary.push(newBook);
-}
-
-
-//loop through array and display the books in cards
-function displayBooks() {
-    shelf.innerText = "";
-
-    myLibrary.forEach((book, index) => {
-        const card = document.createElement("div");
-        card.dataset.index = index;
-    
-        const title = document.createElement("h3");
-        title.innerText = book.name;
-        
-        const author = document.createElement("p");
-        author.innerText = `By ${book.author}`;
-        
-        const pages = document.createElement("p");
-        pages.innerText = `${book.pages} pages`;
-        
-        const readBtn = document.createElement("button");
-        readBtn.innerText = book.read ? "Read" : "Unread";
-        readBtn.classList.add(book.read ? "btn-read" : "btn-unread");
-        readBtn.addEventListener("click", () => toggleRead(index));
-        
-        const deleteBtn = document.createElement("button");
-        deleteBtn.innerText = "Delete";
-        deleteBtn.addEventListener("click", () => removeBook(index));
-
-        const buttonGroup = document.createElement("div");
-        const infoGroup = document.createElement("div");
-
-        shelf.appendChild(card).className = "card";
-        card.appendChild(infoGroup).className = "info-group"
-        card.appendChild(buttonGroup).className = "button-group";
-        infoGroup.appendChild(title).className = "title";
-        infoGroup.appendChild(author).className = "author";
-        infoGroup.appendChild(pages).className = "pages";
-        buttonGroup.appendChild(readBtn);
-        buttonGroup.appendChild(deleteBtn).className = "delete-button";
-    });
-}
-
-// validating form
-const title = document.getElementById("title");
-const author = document.getElementById("author");
-const pages = document.getElementById("pages");
-const err = document.getElementsByClassName("error")
-const titleErr = document.querySelector("#title + span.error")
-const authorErr = document.querySelector("#author + span.error")
-const pagesErr = document.querySelector("#pages + span.error")
-const form = document.getElementById("form");
-
-title.addEventListener("input", (event) => {
-    if (title.validity.valid) {
-        err.textContent = "";
-        err.className = "error";
-    } else {
-        showError();
-    }
-});
-
-form.addEventListener("submit", (event) => {
-    if ((!title.validity.valid) && (!author.validity.valid) && (!pages.validity.valid)) {
-        showError();
-        event.preventDefault;
-    }
-});
-
-function showError() {
-    if (title.validity.valueMissing || author.validity.valueMissing || pages.validity.valueMissing) {
-        err.innerText = "This field can't be empty.";
-    } else if (title.validity.typeMismatch || author.validity.typeMismatch) {
-        titleErr.textContent = "This field is text only.";
-    } else if (pages.validity.typeMismatch) {
-        pagesErr.textContent = "Please enter a number.";
-    }
-
-    err.className = "error active";
-}
-
 // reset form after each fill
 function resetForm() {
     document.getElementById("title").value = "";
@@ -118,19 +99,9 @@ function resetForm() {
     document.getElementById("read").checked = false;
 }
 
-function removeBook(index) {
-    myLibrary.splice(index, 1);
-    displayBooks();
-}
-
-function toggleRead(index) {
-    myLibrary[index].read = !myLibrary[index].read;
-    displayBooks();
-} 
 
 //Update shelf after submit
 submitBtn.addEventListener("click", (e) => {
-
 
     //handling form submission
     e.preventDefault();
@@ -141,15 +112,10 @@ submitBtn.addEventListener("click", (e) => {
     const author = document.getElementById("author").value;
     const pages = document.getElementById("pages").value;
     const read = document.getElementById("read").checked;
-    addBookToLibrary(title, author, pages, read);
+    newBook = new Book(title, author, pages, read);
+    library.addBook(newBook);
     shelf.innerText = "";
-    displayBooks();
+    library.display();
 
     resetForm();
 });
-
-addBookToLibrary("Harry Potter and the Sorcerer's Stone", "J.K. Rowling", 320, true);
-addBookToLibrary("When Breath Becomes Air", "Paul Kalanithi", 228, true);
-addBookToLibrary("Pride and Prejudice", "Jane Austen", 496, false);
-
-displayBooks();
